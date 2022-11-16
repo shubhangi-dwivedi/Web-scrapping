8749d8f1fbbfa38d630fbe7b6aa218effe53171a
Shubhangi Dwivedi <71788610+shubhangi-dwivedi@users.noreply.github.com>
2022-11-16 23:48:43 +0530

    Add files via upload

---
+++ Project-28/Api-1/femi_api.py
@@ -0,0 +1,48 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+from flask_restful import Resource
+from urllib.request import urlopen
+
+class returnjson(Resource):
+    def get(self):
+        seed="apple"
+
+        URL="https://femina.in/search/tag_" + seed
+
+        page = requests.get(URL)
+
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        data1= soup.find_all("div", class_="search-section")
+
+        data=[]
+        for d in data1:
+            short_description = d.find("div", class_="clearfix")
+            title = d.find("h3")
+            snippet = d.find("p")
+            url = title.find("a")["href"]
+
+            short_description=short_description.text.strip("\n")
+
+            data.append({"short_description":short_description,  "title":title.text ,"snippet": snippet.text, "url":url})
+
+            data2={"category": seed, "data": data}
+        jsonData = json.dumps(data2)
+        json_without_slash = json.loads(jsonData)
+
+        return json_without_slash
+
+
+class readjson(Resource):
+    def get(self):
+        url = "http://127.0.0.1:5000/"
+
+        response = urlopen(url)
+
+        data_json = json.loads(response.read())
+
+        with open('femi_data.txt', 'w') as json_file:
+            json.dump(data_json, json_file)
+
+        json_file.close()
